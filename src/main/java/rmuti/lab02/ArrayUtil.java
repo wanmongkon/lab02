821c21fce062d38e48f908a545e8152be5fcd5af
wanmongkon <wanmongkon.ke@kw.ac.th>
2017-12-06 13:41:25 +0700

    s

---
+++ src/main/java/rmuti/lab02/ArrayUtil.java
@@ -0,0 +1,10 @@
+package rmuti.lab02;
+
+public class ArrayUtil {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/rmuti/lab02/TestArray.java
@@ -0,0 +1,10 @@
+package rmuti.lab02;
+
+public class TestArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
